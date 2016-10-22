9b220bdd30a45d20efad5d0725fc7202551cd142
Radosław Dąbrowski <radek.dab@gmail.com>
2016-10-22 13:58:14 +0200

    Draw line in any direction

--- line.cpp
+++ line.cpp
@@ -5,26 +5,60 @@ int Line::counter = 0;
 
 void Line::draw(Raster &rst)
 {
-    int dx = _end.x() - _start.x();
-    int dy = _end.y() - _start.y();
+    int dx, dy;
+    int xi, yi;
 
-    int d = 2*dy - dx;
-    int incrE = 2*dy;
-    int incrNE = 2*(dy-dx);
-    int x = _start.x();
-    int y = _start.y();
+    if (_start.x() < _end.x()) {
+        dx = _end.x() - _start.x();
+        xi = 1;
+    } else {
+        dx = _start.x() - _end.x();
+        xi = -1;
+    }
 
+    if (_start.y() < _end.y()) {
+        dy = _end.y() - _start.y();
+        yi = 1;
+    } else {
+        dy = _start.y() - _end.y();
+        yi = -1;
+    }
+
+    int x = _start.x(), y = _start.y();
     rst.put(x, y, color());
-    while (x < _end.x()) {
-        if (d < 0) {
-            d += incrE;
-            x++;
-        } else {
-            d += incrNE;
-            x++;
-            y++;
+
+    if (dx > dy) {
+        int ai = (dy-dx) << 1;
+        int bi = dy << 1;
+        int d = bi - dx;
+
+        while (x != _end.x()) {
+            if (d >= 0) {
+                x += xi;
+                y += yi;
+                d += ai;
+            } else {
+                x += xi;
+                d += bi;
+            }
+            rst.put(x, y, color());
         }
-        rst.put(x, y, color());
+    } else {
+        int ai = (dx-dy) << 1;
+        int bi = dx << 1;
+        int d = bi - dy;
+
+        while (y != _end.y()) {
+            if (d >= 0) {
+                x += xi;
+                y += yi;
+                d += ai;
+            } else {
+                y += yi;
+                d += bi;
+            }
+            rst.put(x, y, color());
+        }
     }
 }
 
