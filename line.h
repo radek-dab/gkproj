b81349a9fc560e37b2e9a1af7c8228671c0872a0
Radosław Dąbrowski <radek.dab@gmail.com>
2016-10-28 10:04:40 +0200

    Add line thickness getter, setter and assertion

--- line.h
+++ line.h
@@ -6,23 +6,29 @@
 class Line : public Drawable
 {
 public:
-    Line(QPoint start, QPoint end, quint32 color)
+    Line(QPoint start, QPoint end, int thickness, quint32 color)
         : Drawable(QString("Line %1").arg(++counter), color),
           _start(start),
           _end(end),
-          _thickness(1) {}
-    Line(QPoint start, QPoint end, int thickness, quint32 color)
-        : Line(start, end, color) { _thickness = thickness; }
+          _thickness(thickness)
+        { Q_ASSERT(thickness > 0); }
+    Line(QPoint start, QPoint end, quint32 color)
+        : Line(start, end, 1, color) {}
 
-    QPoint start()
+    QPoint start() const
         { return _start; }
     void setStart(const QPoint &start)
         { _start = start; }
 
-    QPoint end()
+    QPoint end() const
         { return _end; }
     void setEnd(const QPoint &end)
         { _end = end; }
+
+    int thickness() const
+        { return _thickness; }
+    void setThickness(int thickness)
+        { Q_ASSERT(thickness > 0); _thickness = thickness; }
 
     void draw(Raster &rst);
     bool hit(const QPoint &p);
