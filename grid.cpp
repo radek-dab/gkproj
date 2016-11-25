67f7eea53f03b603939ec34e8c7b1152dd3bd8d3
Radosław Dąbrowski <radek.dab@gmail.com>
2016-11-25 02:28:34 +0100

    Fix moving grid in clip window

--- grid.cpp
+++ grid.cpp
@@ -5,13 +5,16 @@ void Grid::draw(Raster &rst, QRect clipping)
     if (clipping.isNull())
         clipping = QRect(0, 0, rst.w, rst.h);
 
+    int x0 = clipping.left() + _size - clipping.left() % _size;
+    int y0 = clipping.top() + _size - clipping.top() % _size;
+
     // Draw vertical lines
-    for (int x = clipping.left(); x < clipping.right(); x += _size)
+    for (int x = x0; x < clipping.right(); x += _size)
         for (int y = clipping.top(); y < clipping.bottom(); y++)
-            rst(x, y) = _color;
+            rst.put(x, y, _color);
 
     // Draw horizontal lines
-    for (int y = clipping.top(); y < clipping.bottom(); y += _size)
+    for (int y = y0; y < clipping.bottom(); y += _size)
         for (int x = clipping.left(); x < clipping.right(); x++)
-            rst(x, y) = _color;
+            rst.put(x, y, _color);
 }
